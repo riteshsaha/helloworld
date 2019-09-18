d19282ee0f8779e2c371abd93154e155dfbf735d
riteshsaha <42380085+riteshsaha@users.noreply.github.com>
2019-09-18 13:13:55 +0530

    Update HelloworldApplication.java

--- src/main/java/com/spring/helloworld/api/HelloworldApplication.java
+++ src/main/java/com/spring/helloworld/api/HelloworldApplication.java
@@ -1,6 +1,7 @@
 package com.spring.helloworld.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableAutoConfiguration
